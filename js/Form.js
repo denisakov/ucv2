34a3cd386e179f63fd219bf3737342883cc21a72
Denis Isakov <denisakov@gmail.com>
2018-04-05 19:50:14 +0300

    update of the client list

--- js/Form.js
+++ js/Form.js
@@ -102,7 +102,6 @@
 		}
 		return result;
 	};
-
 	var query = function (sql, callback) {
 		var url = 'https://spreadsheets.google.com/a/google.com/tq?',
 			params = {
@@ -117,8 +116,6 @@
 		url += qs.join('&');
 		return jsonp(url); // Call JSONP helper function
 	};
-
-
 	$('.uc-typeButton').each(function () {
         var value = $(this).val();
         $(this).click(function () {
@@ -133,8 +130,6 @@
             if ($('#submitBtn').hasClass('hidden')) {
                 $('#submitBtn').removeClass('hidden');
             }
-            //$('#testID')[0].parentNode.addClass('input--filled');
-
         });
 
 	});
@@ -144,39 +139,6 @@
 			query("select D", 'getId');
 		}
 	});
-	//Show and Hide the Relevant Forms
-	// $('#uc_generator .documentSelect').change(function () {
-	// 	var selectedOption = $('#uc_generator .documentSelect').val();
-	// 	if (selectedOption === "Hypothesis Document") {
-	// 		$('.hypothesis_form').slideDown('200').addClass('active');
-	// 		$('.idea_form').slideUp('200').removeClass('active');
-	// 		$('.half_sprint_form').slideUp('200').removeClass('active');
-	// 		$('.insight_form').slideUp('200').removeClass('active');
-	// 		$('.sprint_form').slideUp('200').removeClass('active');
-	// 		query("select D", 'getId');
-	// 	} else if (selectedOption === "Test Idea") {
-	// 		$('.idea_form').slideDown('200').addClass('active');
-	// 		$('.hypothesis_form').slideUp('200').removeClass('active');
-	// 		$('.insight_form').slideUp('200').removeClass('active');
-	// 		$('.sprint_form').slideUp('200').removeClass('active');
-	// 		$('.half_sprint_form').slideUp('200').removeClass('active');
-	// 	} else if (selectedOption === "Client Insight") {
-	// 		$('.insight_form').slideDown('200').addClass('active');
-	// 		$('.hypothesis_form').slideUp('200').removeClass('active');
-	// 		$('.idea_form').slideUp('200').removeClass('active');
-	// 		$('.sprint_form').slideUp('200').removeClass('active');
-	// 		$('.half_sprint_form').slideUp('200').removeClass('active');
-	// 	} else if (selectedOption === "Experiment Sprint") {
-	// 		$('.sprint_form').slideDown('200').addClass('active');
-	// 		$('.hypothesis_form').slideUp('200').removeClass('active');
-	// 		$('.idea_form').slideUp('200').removeClass('active');
-	// 		$('.insight_form').slideUp('200').removeClass('active');
-	// 		$('.half_sprint_form').slideUp('200').removeClass('active');
-	// 	}
-	// });
-
-
-
 	//form animation event listener
 	if (!String.prototype.trim) {
 		(function () {
@@ -216,213 +178,176 @@
 			e.target.parentNode.classList.remove('input--filled');
 		}
 	}
-
-	//Show Sprint Forms
-	// $('#yesSprint1').click(function () {
-	// 	$('.half_sprint_form').slideDown('200').addClass('active');
-	// });
-
-
-	// $('#yesSprint2').click(function () {
-	// 	$('.half_sprint_form').slideDown('200').addClass('active');
-	// });
-
-	// $('#noSprint1').click(function () {
-	// 	$('.half_sprint_form').slideUp('200').removeClass('active');
-	// });
-
-
-	// $('#noSprint2').click(function () {
-	// 	$('.half_sprint_form').slideUp('200').removeClass('active');
-	// });
-
-	//document.getElementById("uc_generator").reset();
-
 	// All client data
 	var clientData = {
 		'Agency Central': {
-			prefix: 'AC',
-			colId: 'D',
-			boardId: '5906e420f3a1b5086382a490',
-			listIds: {
-				'Hypothesis Document': '5906e420f3a1b5086382a494',
-				'Test Idea': '5906e420f3a1b5086382a493',
-				'Client Insight': '5906e420f3a1b5086382a491'
-			}
+		  prefix: 'AC',
+		  colId: 'D',
+		  boardId: '5906e420f3a1b5086382a490',
+		  listIds: {
+		    'Hypothesis Document': '5906e420f3a1b5086382a494'
+		  }
 		},
 		'Biscuiteers': {
-			prefix: 'BI',
-			colId: 'D',
-			boardId: '59a88569e1016901c0092d82',
-			listIds: {
-				'Hypothesis Document': '59a88569e1016901c0092d88',
-				'Test Idea': '59a88569e1016901c0092d86',
-				'Client Insight': '59a88569e1016901c0092d85'
-			}
+		  prefix: 'BI',
+		  colId: 'D',
+		  boardId: '59a88569e1016901c0092d82',
+		  listIds: {
+		    'Hypothesis Document': '59a88569e1016901c0092d88'
+		  }
+		},
+		'Blinds Direct': {
+		  prefix: 'BD',
+		  colId: 'D',
+		  boardId: '59f8ee2d345a5ffcc194f8c2',
+		  listIds: {
+		    'Hypothesis Document': '59f8ee2d345a5ffcc194f8c8'
+		  }
 		},
 		'CurrentBody.com': {
-			prefix: 'CB',
-			colId: 'D',
-			boardId: '55e01ee1955d58ca1358cc63',
-			listIds: {
-				'Hypothesis Document': '58492498d28aa56d3b10c7fd',
-				'Test Idea': '55edd6b3a3ee51f0d4f2042a',
-				'Client Insight': '58e2516c9b2b7311701e0f65'
-			}
+		  prefix: 'CB',
+		  colId: 'D',
+		  boardId: '55e01ee1955d58ca1358cc63',
+		  listIds: {
+		    'Hypothesis Document': '58492498d28aa56d3b10c7fd',
+		    'Test Idea': '55edd6b3a3ee51f0d4f2042a',
+		    'Client Insight': '58e2516c9b2b7311701e0f65'
+		  }
 		},
-
+		'Flannels': {
+		  prefix: 'FL',
+		  colId: 'D',
+		  boardId: '5aa70888a2f9ef5b544c6517',
+		  listIds: {
+		    'Hypothesis Document': '5aa70888a2f9ef5b544c6455'
+		  }
+		},
+		'Glasses Direct': {
+		  prefix: 'GD',
+		  colId: 'D',
+		  boardId: '5ac472ca290997a69f95ed3b',
+		  listIds: {
+		    'Hypothesis Document': '5ac472ca290997a69f95ed43'
+		  }
+		},
+		'Hearing Direct': {
+		  prefix: 'HD',
+		  colId: 'D',
+		  boardId: '5a6f9d357e0ea72ded1dd7a6',
+		  listIds: {
+		    'Hypothesis Document': '5a6f9d357e0ea72ded1dd7ae'
+		  }
+		},
 		'In The Style': {
-			prefix: 'IT',
-			colId: 'D',
-			boardId: '59402d04865d955bc5bd78a3',
-			listIds: {
-				'Hypothesis Document': '59402d04865d955bc5bd78a9',
-				'Test Idea': '59402d04865d955bc5bd78a7',
-				'Client Insight': '59402d04865d955bc5bd78a6'
-			}
+		  prefix: 'IT',
+		  colId: 'D',
+		  boardId: '59402d04865d955bc5bd78a3',
+		  listIds: {
+		    'Hypothesis Document': '5a6f9d357e0ea72ded1dd7ae'
+		  }
 		},
 		'Mamas & Papas': {
-			prefix: 'MP',
-			colId: 'D',
-			boardId: '57f7abe044ac7abe391dc181',
-			listIds: {
-				'Hypothesis Document': '581afc61a6d37ac1f71c8e23',
-				'Test Idea': '58048785d1e555e8a3bd4f46',
-				'Client Insight': '58ff6d846b638b4a47fdbba3'
-			}
+		  prefix: 'MP',
+		  colId: 'D',
+		  boardId: '57f7abe044ac7abe391dc181',
+		  listIds: {
+		    'Hypothesis Document': '581afc61a6d37ac1f71c8e23'
+		  }
 		},
 		'Merchoid': {
-			prefix: 'ME',
-			colId: 'D',
-			boardId: '561668d9af8166f38e581c38',
-			listIds: {
-				'Hypothesis Document': '58275cc19999dd7339c70f1a',
-				'Test Idea': '561668d9af8166f38e581c3d',
-				'Client Insight': '590773a1a203842a0a72477b'
-			}
+		  prefix: 'ME',
+		  colId: 'D',
+		  boardId: '561668d9af8166f38e581c38',
+		  listIds: {
+		    'Hypothesis Document': '58275cc19999dd7339c70f1a'
+		  }
 		},
 		'National Holidays': {
-			prefix: 'NH',
-			colId: 'D',
-			boardId: '59b2abfa2bc0ee9833921360',
-			listIds: {
-				'Hypothesis Document': '59b2abfa2bc0ee9833921366',
-				'Test Idea': '59b2abfa2bc0ee9833921364',
-				'Client Insight': '59b2abfa2bc0ee9833921363'
-			}
+		  prefix: 'NH',
+		  colId: 'D',
+		  boardId: '59b2abfa2bc0ee9833921360',
+		  listIds: {
+		    'Hypothesis Document': '59b2abfa2bc0ee9833921366'
+		  }
+		},
+		'Papa Johns': {
+		  prefix: 'PJ',
+		  colId: 'D',
+		  boardId: '5a1be6f78c43043ab0337f76',
+		  listIds: {
+		    'Hypothesis Document': '5a1be6f78c43043ab0337f7c'
+		  }
+		},
+		'Printerland': {
+		  prefix: 'PL',
+		  colId: 'D',
+		  boardId: '5a9d19a738c7f5f6be0899f8',
+		  listIds: {
+		    'Hypothesis Document': '5a9d19a738c7f5f6be089a00'
+		  }
 		},
 		'Protec Direct': {
-			prefix: 'PD',
-			colId: 'D',
-			boardId: '597a159e22addb53d82dbed8',
-			listIds: {
-				'Hypothesis Document': '597a159e22addb53d82dbede',
-				'Test Idea': '597a159e22addb53d82dbedc',
-				'Client Insight': '597a159e22addb53d82dbedb'
-			}
+		  prefix: 'PD',
+		  colId: 'D',
+		  boardId: '597a159e22addb53d82dbed8',
+		  listIds: {
+		    'Hypothesis Document': '597a159e22addb53d82dbede'
+		  }
 		},
-
 		'Red Cross Training': {
-			prefix: 'RC',
-			colId: 'D',
-			boardId: '58bfe0d039fbf3767dfa7c74',
-			listIds: {
-				'Hypothesis Document': '58bfe0d039fbf3767dfa7c77',
-				'Test Idea': '58bfe0d039fbf3767dfa7c76',
-				'Client Insight': '58e8a9250847c82477ee6aa2'
-			}
+		  prefix: 'RC',
+		  colId: 'D',
+		  boardId: '58bfe0d039fbf3767dfa7c74',
+		  listIds: {
+		    'Hypothesis Document': '58bfe0d039fbf3767dfa7c77'
+		  }
 		},
 		'Salons Direct': {
-			prefix: 'SD',
-			colId: 'D',
-			boardId: '585c80bcb8ad0c98f00d2af8',
-			listIds: {
-				'Hypothesis Document': '585c80da6ff11dcf1e408913',
-				'Test Idea': '585c80cbf71c88463409181d',
-				'Client Insight': '58f5e5db56bfadbd30a1981e'
-			}
+		  prefix: 'SD',
+		  colId: 'D',
+		  boardId: '585c80bcb8ad0c98f00d2af8',
+		  listIds: {
+		    'Hypothesis Document': '585c80da6ff11dcf1e408913'
+		  }
 		},
-		'Soak & Sleep': {
-			prefix: 'SO',
-			colId: 'D',
-			boardId: '59074d9d356212551befb036',
-			listIds: {
-				'Hypothesis Document': '59074d9d356212551befb03a',
-				'Test Idea': '59074d9d356212551befb039',
-				'Client Insight': '59074d9d356212551befb037'
-			}
-		},
-
 		'Technogym': {
-			prefix: 'TG',
-			colId: 'D',
-			boardId: '595a0936410a8716ca62e06b',
-			listIds: {
-				'Hypothesis Document': '595a097830575e396c287c5c',
-				'Test Idea': '595a095f853628d8234df5d8',
-				'Client Insight': '595a095acb4f9ae08c413f28'
-			}
+		  prefix: 'TG',
+		  colId: 'D',
+		  boardId: '595a0936410a8716ca62e06b',
+		  listIds: {
+		    'Hypothesis Document': '595a097830575e396c287c5c'
+		  }
 		},
 		'Travis Perkins': {
-			prefix: 'TP',
-			colId: 'D',
-			boardId: '58d0f0a305ce7f0715d7cda1',
-			listIds: {
-				'Hypothesis Document': '58d0f0a305ce7f0715d7cda4',
-				'Test Idea': '58d0f0a305ce7f0715d7cda3',
-				'Client Insight': '58f67ec53e632c452395e041'
-			}
+		  prefix: 'TP',
+		  colId: 'D',
+		  boardId: '58d0f0a305ce7f0715d7cda1',
+		  listIds: {
+		    'Hypothesis Document': '58d0f0a305ce7f0715d7cda4'
+		  }
 		},
 		'Wolf & Badger': {
-			prefix: 'WB',
-			colId: 'D',
-			boardId: '57357af07c6ddfe1e3d59822',
-			listIds: {
-				'Hypothesis Document': '5756cabb535e401a068c6def',
-				'Test Idea': '5757dcc5671127c31081aa11',
-				'Client Insight': '59076efc5087c251e65af381'
-			}
+		  prefix: 'WB',
+		  colId: 'D',
+		  boardId: '57357af07c6ddfe1e3d59822',
+		  listIds: {
+		    'Hypothesis Document': '5756cabb535e401a068c6def'
+		  }
 		},
 		'Wooden Blinds Direct': {
-			prefix: 'WO',
-			colId: 'D',
-			boardId: '59074cd57d56688f67752eb1',
-			listIds: {
-				'Hypothesis Document': '59074cd57d56688f67752eb5',
-				'Test Idea': '59074cd57d56688f67752eb4',
-				'Client Insight': '59074cd57d56688f67752eb2'
-			}
+		  prefix: 'WO',
+		  colId: 'D',
+		  boardId: '59074cd57d56688f67752eb1',
+		  listIds: {
+		    'Hypothesis Document': '59074cd57d56688f67752eb5'
+		  }
 		}
 	};
-
-
-
-	/* thisClientData will be populated when user has selected both a document
-	type and client from the dropdowns.
-	@example:
-	{
-		name: 'Agency Central',
-		data: {
-			prefix: 'AC',
-			colId: 'BI',
-			boardId: '0000000042',
-			listIds: {
-				'Hypothesis Document': '0000000000',
-				'Test Idea': '0000000001',
-				'Client Insight': '0000000002'
-			},
-			testId: '002'
-		}
-	}
-	*/
 	var thisClientData = {};
-
-
 	window.getId = function (data) {
 		data = parse(data);
 		var newId;
 		var clientPrefix = thisClientData.data.prefix;
-
 		/* loop backwards through array and find last entry with a number.
 		add 1 to this number to generate ID for this test */
 		for (var i = data.length; i >= 0; i--) {
@@ -461,35 +386,8 @@
 
 			if (i === 0) newId = clientPrefix + '001';
 		}
-
-		//$('#showTestId').html(newId);
-		// Update hidden input to send to Google Sheets on submit
 		$('#testID').val(newId);
-		//$('#testerId').val(newId);
-		// $('#testID').attr("placeholder",newId);
 	};
-
-	// $('#testID').focus(function (e) {
-	// 	//console.log(thisClientData);
-	// 	e.preventDefault();
-	// 	if (thisClientData && thisClientData.data && thisClientData.data.colId) {
-	// 		//getTestId();
-	// 		 query("select " + thisClientData.data.colId, 'getId');
-	// 	} else {
-	// 		console.error('Failed to get ID - No client data available');
-	// 	}
-	// });
-	// $('#getTestId').click(function (e) {
-	// 	//console.log(thisClientData);
-	// 	e.preventDefault();
-	// 	if (thisClientData && thisClientData.data && thisClientData.data.colId) {
-	// 		//getTestId();
-	// 		 query("select " + thisClientData.data.colId, 'getId');
-	// 	} else {
-	// 		console.error('Failed to get ID - No client data available');
-	// 	}
-	// });
-
 	var updateThisClientData = function () {
 		//var docType = $('#docType').val();
 		var client = $('#clientName1').val();
@@ -498,48 +396,19 @@
 			thisClientData.data = clientData[client];
 			thisClientData.docType = 'Hypothesis Document';
 			thisClientData.prefix = clientData[client].prefix;
-			// $('#testID').value = clientData[client].prefix;
-			// console.log('Prefix assiged: ' + $('#testID').val());
-
-			// Show generate ID button if hypothesis document
-			// if (docType === 'Hypothesis Document') {
-			// 	$('#getTestId').show();
-			// } else {
-			// 	$('#getTestId').hide();
-			// }
 		}
 	};
-
 	// On change, if both inputs have been selected, update hidden inputs
 	$('#clientName1').change(updateThisClientData);
-
 	function clearForm() {
 		$('#uc_generator')[0].reset();
-		// $('.hypothesis_form').removeClass('active');
-		// $('.idea_form').removeClass('active');
-		// $('.half_sprint_form').removeClass('active');
-		// $('.insight_form').removeClass('active');
-		// $('.sprint_form').removeClass('active');
-		// $('#submitBtn')[0].disabled;
-		// $('#submitBtn').prop('disabled', false);
-		// $('#submitBtn').addClass('hidden');
-		// $('.uc-typeButton').removeClass('active');
-		// $('.second_section').slideUp('200');
-		// $('#yesSprint2')[0].checked = false;
-		// $('#yesSprint1')[0].checked = false;
-		// $('#noSprint1')[0].checked = false;
-		// $('#noSprint2')[0].checked = false;
-
 		var $filled = $('.input--filled');
 		if ($filled.length) {
 			$filled.removeClass('input--filled');
 		}
 	}
-
 	$('#uc_generator').submit(function (e) {
 		e.preventDefault();
-		//console.log('The button is clicked');
-		// update hidden inputs
 		var docType = thisClientData.docType;
 		var listId = thisClientData.data.listIds[docType];
 		var boardId = thisClientData.data.boardId;
@@ -548,33 +417,9 @@
 		$('#boardID').val(boardId);
 		$('#prefix').val(prefix);
 		$('#docType').val(docType);
-
-		/* If Hypothesis Document make sure generated ID exists and selected client id matches generated ID */
-		// if (thisClientData.docType === 'Hypothesis Document') {
-		// 	var selectedClientID = thisClientData.data.prefix;
-		// 	var generatedID = $('#showTestId').text();
-		// 	if (!generatedID) {
-		// 		alert('Please generate an ID before submitting');
-		// 		return false;
-		// 	} else if (generatedID && generatedID.match(/\w{2}/)[0] !== selectedClientID) {
-		// 		alert('Please generate a new ID for this client before submitting');
-		// 		return false;
-		// 	}
-		// }
-
 		formSubmit();
-
-		// $("#submitBtn").prop('disabled', true);
-
 		setTimeout(function () {
-
-			// var r = confirm("Form Submitted. You can now close the window or select \"OK\" to create a new form");
-			// if (r == true) {
-				clearForm();
-
-			// }
-
+			clearForm();
 		}, 500)
 	});
-
 })();
